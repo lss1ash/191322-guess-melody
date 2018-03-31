cab5f0495c21b8934e6181a4c628b987776dd088
Alexander <lss1ash@rambler.ru>
2018-04-01 01:26:19 +1100

    Добавляет модули шаблонов страниц

---
+++ js/templates/resultAttemptsLeft.js
@@ -0,0 +1,13 @@
+import getElementFromString from '../getElementFromString.js';
+
+// <!-- Результат игры: проигрыш закончились попытки -->
+
+const template = `<section class="main main--result">
+  <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+
+  <h2 class="title">Какая жалость!</h2>
+  <div class="main-stat">У вас закончились все попытки.<br>Ничего, повезёт в следующий раз!</div>
+  <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
+</section>`;
+
+export default getElementFromString(template);
