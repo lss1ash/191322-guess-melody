da2120e76f5ec72822b2533c982745bba7e232ff
Alexander <lss1ash@rambler.ru>
2018-04-06 02:19:17 +1100

    добавляет тесты для функции получения результата (суммы очков)

--- js/data/result.js
+++ js/data/result.js
@@ -1,5 +1,22 @@
-const calculateResult = () => {
 
+const Options = {
+  QUESTIONS_NUMBER: 10, // требуемое количество ответов
+  ANSWER_SPEED: 30, // скорость быстрого ответа на вопрос
+  MAX_TRIES: 3
+};
+
+const calculateResult = (answers, notesLeft) => {
+
+  if (!Array.isArray(answers) || answers.length !== Options.QUESTIONS_NUMBER) {
+    return -1;
+  }
+
+  if (!Number.isInteger(notesLeft)) {
+    return -1;
+  }
+
+  const result = answers.reduce((accumulator, {right, fast}) => Number(accumulator) + Number(right) + Number(fast), 0);
+  return result - (Options.MAX_TRIES - notesLeft) * 2;
 };
 
 export default calculateResult;
--- js/data/result.test.js
+++ js/data/result.test.js
@@ -2,9 +2,9 @@ import {assert} from 'chai';
 import calculateResult from './result';
 
 describe(`Функция подсчёта набранных баллов игрока`, () => {
-  describe(`Функция на вход принимает массив ответов пользователя`, () => {
 
-    it(`возвращает -1 в том случае, если первый параметр - не массив `, () => {
+  describe(`Функция на вход принимает массив ответов пользователя`, () => {
+    it(`возвращает -1, если первый параметр - не массив `, () => {
       assert.equal(-1, calculateResult());
       assert.equal(-1, calculateResult(1));
       assert.equal(-1, calculateResult(true));
@@ -13,13 +13,88 @@ describe(`Функция подсчёта набранных баллов игр
       assert.equal(-1, calculateResult({}));
     });
 
-    it(`возвращает -1 в том случае, если второй параметр - не число `, () => {
+    it(`возвращает -1, если количество элементов массива с ответами !== 10 `, () => {
+      assert.equal(-1, calculateResult(new Array(0)));
+      assert.equal(-1, calculateResult(new Array(1)));
+      assert.equal(-1, calculateResult(new Array(2)));
+      assert.equal(-1, calculateResult(new Array(3)));
+      assert.equal(-1, calculateResult(new Array(4)));
+      assert.equal(-1, calculateResult(new Array(5)));
+      assert.equal(-1, calculateResult(new Array(6)));
+      assert.equal(-1, calculateResult(new Array(7)));
+      assert.equal(-1, calculateResult(new Array(8)));
+      assert.equal(-1, calculateResult(new Array(9)));
+      assert.equal(-1, calculateResult(new Array(11)));
+      assert.equal(-1, calculateResult(new Array(22)));
+      assert.equal(-1, calculateResult(new Array(66)));
+    });
+  });
+
+  describe(`Функция на вход принимает массив ответов пользователя`, () => {
+    it(`возвращает -1, если второй параметр - не число `, () => {
       assert.equal(-1, calculateResult([]));
       assert.equal(-1, calculateResult([], `123`));
+      assert.equal(-1, calculateResult([], 12.3));
       assert.equal(-1, calculateResult([], NaN));
       assert.equal(-1, calculateResult([], true));
       assert.equal(-1, calculateResult([], []));
       assert.equal(-1, calculateResult([], {}));
     });
+  });
+
+  describe(`За правильный ответ 1 балл;\nЗа быстрый правильный ответ (менее 30 секунд) — 2 балла;\nЗа каждую ошибку вычитается 2 балла.`, () => {
+
+    it(`возвращает 10, если всё правильно, ошибок нет и все ответы не быстрые`, () => {
+      assert.equal(10, calculateResult([{right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false}], 3));
+    });
+
+    it(`возвращает 8, если всё правильно, 1 ошибка и все ответы не быстрые`, () => {
+      assert.equal(8, calculateResult([{right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false}], 2));
+    });
+
+    it(`возвращает 6, если всё правильно, 2 ошибки и все ответы не быстрые`, () => {
+      assert.equal(6, calculateResult([{right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false}], 1));
+    });
+
+    it(`возвращает 3, если 9 правильно, 3 ошибки и все ответы не быстрые`, () => {
+      assert.equal(3, calculateResult([{right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: true, fast: false},
+        {right: false, fast: false}], 0));
+    });
+
   });
 });
