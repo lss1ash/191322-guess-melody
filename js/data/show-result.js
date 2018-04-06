20dc18d77376fbd63b57de23f56c2cfed448e8de
Alexander <lss1ash@rambler.ru>
2018-04-06 16:56:39 +1100

    Промежуточный коммит. Сохраним работу

--- js/data/show-result.js
+++ js/data/show-result.js
@@ -10,6 +10,13 @@ const showResult = (previousScores, result = {}) => {
     return -1;
   }
 
+  if (notesLeft === 0) {
+    return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+  }
+
+  if (timeLeft === 0) {
+    return `Время вышло! Вы не успели отгадать все мелодии`;
+  }
   // const result = answers.reduce((accumulator, {right, fast}) => Number(accumulator) + Number(right) + Number(right && fast), 0);
   // return result - (Options.MAX_TRIES - notesLeft) * 2;
   return {currentScore, notesLeft, timeLeft};
