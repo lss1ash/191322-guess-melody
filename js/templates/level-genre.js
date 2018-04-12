cb7f1b663a31baa78802fddc46fca5853cc6362c
Alexander <lss1ash@rambler.ru>
2018-04-12 18:14:35 +1100

    Промежуточный коммит

--- js/templates/level-genre.js
+++ js/templates/level-genre.js
@@ -5,128 +5,94 @@ import resultTimeLeftTemplate from './result-time-left';
 
 // <!-- Игра на выбор жанра -->
 
-const template = `<section class="main main--level main--level-genre">
-  <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-    <circle
-      cx="390" cy="390" r="370"
-      class="timer-line"
-      style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-
-    <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-      <span class="timer-value-mins">05</span><!--
-      --><span class="timer-value-dots">:</span><!--
-      --><span class="timer-value-secs">00</span>
+const getSongMarkup = (song, number) => {
+  return `<div class="genre-answer">
+    <div class="player-wrapper">
+      <div class="player">
+        <audio></audio>
+        <button class="player-control player-control--pause"></button>
+        <div class="player-track">
+          <span class="player-status"></span>
+        </div>
+      </div>
     </div>
-  </svg>
-  <div class="main-mistakes">
-    <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-    <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-    <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-  </div>
-
-  <div class="main-wrap">
-    <h2 class="title">Выберите инди-рок треки</h2>
-    <form class="genre">
-      <div class="genre-answer">
-        <div class="player-wrapper">
-          <div class="player">
-            <audio></audio>
-            <button class="player-control player-control--pause"></button>
-            <div class="player-track">
-              <span class="player-status"></span>
-            </div>
-          </div>
-        </div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-1">
-        <label class="genre-answer-check" for="a-1"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper">
-          <div class="player">
-            <audio></audio>
-            <button class="player-control player-control--play"></button>
-            <div class="player-track">
-              <span class="player-status"></span>
-            </div>
-          </div>
-        </div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-2">
-        <label class="genre-answer-check" for="a-2"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper">
-          <div class="player">
-            <audio></audio>
-            <button class="player-control player-control--play"></button>
-            <div class="player-track">
-              <span class="player-status"></span>
-            </div>
-          </div>
-        </div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-3">
-        <label class="genre-answer-check" for="a-3"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper">
-          <div class="player">
-            <audio></audio>
-            <button class="player-control player-control--play"></button>
-            <div class="player-track">
-              <span class="player-status"></span>
-            </div>
-          </div>
-        </div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-4">
-        <label class="genre-answer-check" for="a-4"></label>
-      </div>
-
-      <button class="genre-answer-send" disabled type="submit">Ответить</button>
-    </form>
-  </div>
-</section>`;
-
-const levelGenrePage = getElementFromString(template);
-const genreBoxes = levelGenrePage.querySelectorAll(`.genre-answer input[type=checkbox]`);
-const answer = levelGenrePage.querySelector(`.genre-answer-send`);
-
-const changeBoxCheckedHandler = () => {
-  let checked = false;
-  [...genreBoxes].forEach((checkbox) => {
-    checked = checked || checkbox.checked;
-  });
-  answer.disabled = !checked;
+    <input type="checkbox" name="answer" value="answer-${number}" id="a-${number}">
+    <label class="genre-answer-check" for="a-${number}"></label>
+  </div>`;
 };
 
-const answerClickHandler = (e) => {
-  e.preventDefault();
+export default (answers) => {
 
-  answer.disabled = true;
-  [...genreBoxes].forEach((checkbox) => {
-    checkbox.checked = false;
-  });
+  const template = `<section class="main main--level main--level-genre">
+    <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+      <circle
+        cx="390" cy="390" r="370"
+        class="timer-line"
+        style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
 
-  const result = {
-    minutes: 3,
-    seconds: 12,
-    score: 10,
-    scoreFast: 10,
-    mistakes: 2,
-    place: 3,
-    placesAll: 15,
-    betterPercent: 66
-  };
+      <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+        <span class="timer-value-mins">05</span><!--
+        --><span class="timer-value-dots">:</span><!--
+        --><span class="timer-value-secs">00</span>
+      </div>
+    </svg>
+    <div class="main-mistakes">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+    </div>
 
-  switch (Math.floor(getRandomArbitrary(0, 3))) {
-    case 0: drawPage(resultTemplate(result)); break;
-    case 1: drawPage(resultAttemptsLeftTemplate); break;
-    case 2: drawPage(resultTimeLeftTemplate); break;
-  }
+    <div class="main-wrap">
+      <h2 class="title">Выберите инди-рок треки</h2>
+      <form class="genre">
+
+        <button class="genre-answer-send" disabled type="submit">Ответить</button>
+      </form>
+    </div>
+  </section>`;
+
+  const levelGenrePageElement = getElementFromString(template);
+
+  // const genreBoxesElements = levelGenrePage.querySelectorAll(`.genre-answer input[type=checkbox]`);
+  const sendButton = levelGenrePageElement.querySelector(`.genre-answer-send`);
+
+  // const changeBoxCheckedHandler = () => {
+  //   let checked = false;
+  //   [...genreBoxes].forEach((checkbox) => {
+  //     checked = checked || checkbox.checked;
+  //   });
+  //   answer.disabled = !checked;
+  // };
+  //
+  // const answerClickHandler = (e) => {
+  //   e.preventDefault();
+  //
+  //   answer.disabled = true;
+  //   [...genreBoxes].forEach((checkbox) => {
+  //     checkbox.checked = false;
+  //   });
+  //
+  //   const result = {
+  //     minutes: 3,
+  //     seconds: 12,
+  //     score: 10,
+  //     scoreFast: 10,
+  //     mistakes: 2,
+  //     place: 3,
+  //     placesAll: 15,
+  //     betterPercent: 66
+  //   };
+  //
+  //   switch (Math.floor(getRandomArbitrary(0, 3))) {
+  //     case 0: drawPage(resultTemplate(result)); break;
+  //     case 1: drawPage(resultAttemptsLeftTemplate); break;
+  //     case 2: drawPage(resultTimeLeftTemplate); break;
+  //   }
+  // };
+
+  // [...genreBoxes].forEach((checkbox) => checkbox.addEventListener(`change`, changeBoxCheckedHandler));
+  // answer.addEventListener(`click`, answerClickHandler);
+
+//export default levelGenrePage;
+
 };
-
-[...genreBoxes].forEach((checkbox) => checkbox.addEventListener(`change`, changeBoxCheckedHandler));
-answer.addEventListener(`click`, answerClickHandler);
-
-export default levelGenrePage;
