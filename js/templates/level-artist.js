49220ed82ae74033fe410f8f8683c39d2c09ff18
Alexander <lss1ash@rambler.ru>
2018-04-13 01:54:14 +1100

    Шаблонизируем

--- js/templates/level-artist.js
+++ js/templates/level-artist.js
@@ -27,18 +27,7 @@ const formClickHandler = ({target}) => {
 export default (answers) => {
 
   const template = `<section class="main main--level main--level-artist">
-  <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-    <circle
-      cx="390" cy="390" r="370"
-      class="timer-line"
-      style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-
-    <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-      <span class="timer-value-mins">05</span><!--
-      --><span class="timer-value-dots">:</span><!--
-      --><span class="timer-value-secs">00</span>
-    </div>
-  </svg>
+    ${svgTemplate}
     ${getMistakesTemplate(2)}
     <div class="main-wrap">
       <h2 class="title main-title">Кто исполняет эту песню?</h2>
