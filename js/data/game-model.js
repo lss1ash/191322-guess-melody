107adc21c3514b98d42886bc689f6fd6b49a056d
Alexander <lss1ash@rambler.ru>
2018-04-21 14:53:07 +1100

    Формирует модель игры

--- js/data/game-model.js
+++ js/data/game-model.js
@@ -11,7 +11,8 @@ const Options = {
   GENRE: `levelGenre`,
   TOTAL_QUESTIONS: 10,
   ANSWER_SPEED: 30,
-  MISTAKES_TO_LOOSE: 3
+  MISTAKES_TO_LOOSE: 3,
+  TOTAL_TIME: 600
 };
 
 const createLevel = (levelType = getRandom(0, 2) === 0 ? Options.ARTIST : Options.GENRE) => {
@@ -84,53 +85,41 @@ export default class GameModel {
     };
   }
 
-  set answer(currentAnswer) {
-
+  get hasNextLevel() {
+    if (this.state.mistakes === Options.MISTAKES_TO_LOOSE) {
+      return false;
+    }
+    if (this.state.time >= Options.TOTAL_TIME) {
+      return false;
+    }
+    if (this.state.currentLevel === Options.TOTAL_QUESTIONS - 1) {
+      return false;
+    }
+    return true;
   }
 
-  get nextLevel() {
-
+  _checkAnswer(currentAnswer) {
+    this.state.userAnswers.push(currentAnswer);
+    if (!this._isCorrectAnswer()) {
+      this.state = {mistakes: this.state.mistakes + 1};
+    }
   }
 
+  getNextLevel(currentAnswer) {
+    if (this.state.currentLevel > 0) {
+      this._checkAnswer(currentAnswer);
+    }
+    if (this.hasNextLevel) {
+      const level = this.state.levels[this.state.currentLevel];
+      this.state = {currentLevel: this.state.currentLevel + 1};
+      return level;
+    }
+    return false;
+  }
 
+  _isCorrectAnswer() {
+    const currentLevel = this.state.levels[this.state.currentLevel - 1];
+    const userAnswers = this.state.userAnswers[this.state.currentLevel - 1];
+    return userAnswers.every((value, index) => currentLevel.answers[index].right === value);
+  }
 }
-
-
-// class Game {
-//
-//
-//   nextLevel(answer) {
-//     if (this.state.currentLevel > 0) {
-//       this.testCurrentAnswer(answer);
-//     }
-//     if (this.state.mistakes === Options.MISTAKES_TO_LOOSE) {
-//       drawPage(getResultAttemptsLeft().element);
-//       return;
-//     }
-//     const level = this.state.currentLevel < Options.TOTAL_QUESTIONS ? this.state.levels[this.state.currentLevel] : false;
-//     if (level) {
-//       this.state = {currentLevel: this.state.currentLevel + 1};
-//
-//       switch (level.type) {
-//         case Options.GENRE: drawPage(getLevelGenre(level).element); break;
-//         case Options.ARTIST: drawPage(getLevelArtist(level).element); break;
-//       }
-//       return;
-//     }
-//     drawPage(getResultSuccess(this.result));
-//   }
-//
-//   testUserAnswer() {
-//     const currentLevel = this.state.levels[this.state.currentLevel - 1];
-//     const userAnswers = this.state.userAnswers[this.state.currentLevel - 1];
-//     return userAnswers.every((value, index) => currentLevel.answers[index].right === value);
-//   }
-//
-//   testCurrentAnswer(userAnswer) {
-//     this.state.userAnswers.push(userAnswer);
-//     if (!this.testUserAnswer()) {
-//       this.state = {mistakes: this.state.mistakes + 1};
-//     }
-//   }
-//
-// }
