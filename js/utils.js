1236863dcc7c56fc97abdd90c9b7135d1824f504
Alexander <lss1ash@rambler.ru>
2018-05-06 22:36:11 +0300

    Удаляет неиспользуемый код shuffleArray

--- js/utils.js
+++ js/utils.js
@@ -11,16 +11,6 @@ export const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-export const shuffleArray = (array) => {
-  for (let index in array) {
-    if (Object.prototype.hasOwnProperty.call(array, index)) {
-      const randomIndex = getRandom(0, array.length);
-      [array[randomIndex], array[index]] = [array[index], array[randomIndex]];
-    }
-  }
-  return array;
-};
-
 export const getMinSec = (time) => {
   const minutes = Math.floor(time / SECONDS_PER_MINUTE);
   const seconds = time - (minutes * SECONDS_PER_MINUTE);
